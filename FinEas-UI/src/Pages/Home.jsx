6f95c9a601e0b4146072a9ea42a9aca5bddb9d3b
hakimcolor <hakimcolor777@gmail.com>
2025-11-26 23:13:51 +0600

    my slider

--- FinEas-UI/src/Pages/Home.jsx
+++ FinEas-UI/src/Pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import HomeSlider from '../Componentes/HomeSlider';
 
 const Home = () => {
   return (
     <div>
+      <HomeSlider />
       it's a home page ok ..os you can see the home page as a home page ok sir
       .. now next ok
     </div>
